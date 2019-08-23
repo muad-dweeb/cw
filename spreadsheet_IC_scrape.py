e40b8bd3ea733c8e053a88ab6573941f2a645d96
Andrew Galloway <akq.galloway@gmail.com>
2019-08-23 16:08:05 -0700

    separates ICScraper CLI from class

---
+++ spreadsheet_IC_scrape.py
@@ -0,0 +1,24 @@
+from selenium.common.exceptions import NoSuchWindowException
+
+from ICScraper import ICScraper
+from exceptions import ScraperException
+
+if __name__ == '__main__':
+    scraper = None
+    try:
+        scraper = ICScraper('~/muad-dweeb/cw/config/ic_creds.json')  # TODO: remove hardcoded path
+        # TODO: Maybe login should just be a fully manual process... no creds file involved?
+        scraper.login()
+
+        # TESTING
+        contact_info = scraper.get_all_info('andrew', 'galloway', 'seattle', 'wa')
+        print(contact_info)
+
+    except ScraperException as e:
+        print('Scrape failed. Error: {}'.format(e))
+
+    except NoSuchWindowException:
+        print('Window was closed prematurely')
+
+    if scraper:
+        scraper.close()
