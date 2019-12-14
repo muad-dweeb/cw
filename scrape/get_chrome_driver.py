5c1671bc7da1f6926bf8ac06ae3578d0fa76eea9
Andrew Galloway <akq.galloway@gmail.com>
2019-12-14 14:17:34 -0800

    adds chrome driver installer skeleton for later use

---
+++ scrape/get_chrome_driver.py
@@ -0,0 +1,18 @@
+"""
+TODO: DO THE DEW
+"""
+
+
+def acquire_version():
+    """ get the version of chrome installed """
+    pass
+
+
+def download(version):
+    """ download the corresponding chromedriver version """
+    pass
+
+
+def install():
+    """ symlink probably, for swapability """
+    pass
