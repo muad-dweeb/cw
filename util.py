a5c1ced064612bb0b56820fbbee882c40e9732fd
Andrew Galloway <akq.galloway@gmail.com>
2020-01-08 16:09:12 -0800

    adds logger helper function

--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 from os import path
 
@@ -21,3 +22,21 @@ def create_new_filename(in_path, overwrite_existing=False):
             increment += 1
 
     return out_path
+
+
+def create_logger(caller, debug=False):
+    """ General purpose logger with simple configuration """
+    if debug:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+
+    new_logger = logging.getLogger(caller)
+    console_handler = logging.StreamHandler()
+    log_format = logging.Formatter('%(asctime)s - %(funcName)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(log_format)
+    new_logger.addHandler(console_handler)
+    new_logger.setLevel(log_level)
+    # Prevent duplicate log entries if another handler exists downstream: https://stackoverflow.com/a/44426266/3900915
+    new_logger.propagate = False
+    return new_logger
