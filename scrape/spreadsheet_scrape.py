a0d4c7fae460285cd241cfa0d060cad8f81ba81f
Andrew Galloway <akq.galloway@gmail.com>
2020-03-28 19:09:39 -0700

    converts log cache into text block

--- scrape/spreadsheet_scrape.py
+++ scrape/spreadsheet_scrape.py
@@ -436,9 +436,10 @@ def main(config_path, site, environment, limit_rows=None, limit_minutes=None, au
 
     # Send email report
     if email_report:
+        output_tail = '\n'.join(logger.cache)
         try:
             reporter = EmailReporter(sender=run_config.email_sender, recipient=run_config.email_recipient)
-            reporter.send_report(metrics_dict=metrics, sample_output=logger.cache)
+            reporter.send_report(metrics_dict=metrics, sample_output=output_tail)
         except EmailReporterException as e:
             logger.exception('Failed to send email: {}'.format(e))
 
